2b2d72831dfe321a6d0b1afddf0a3794026bc371
Rodrigo Porto <rodrigoandradeporto@hotmail.com>
2025-05-22 10:45:05 -0300

    Initial Commit

---
+++ MeshMate/MeshMateApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeshMateApp.swift
+//  MeshMate
+//
+//  Created by Rodrigo Porto on 22/05/25.
+//
+
+import SwiftUI
+
+@main
+struct MeshMateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
