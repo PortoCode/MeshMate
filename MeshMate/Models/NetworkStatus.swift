cb9b78c4fd666f1968df45d544c3058d7b78d04c
Rodrigo Porto <rodrigoandradeporto@hotmail.com>
2025-05-25 20:06:28 -0300

    feat: add NetworkStatus model

---
+++ MeshMate/Models/NetworkStatus.swift
@@ -0,0 +1,15 @@
+//
+//  NetworkStatus.swift
+//  MeshMate
+//
+//  Created by Rodrigo Porto on 25/05/25.
+//
+
+import Foundation
+
+struct NetworkStatus {
+    let isOnline: Bool
+    let latency: Int
+    let downloadSpeedMbps: Double
+    let uploadSpeedMbps: Double
+}
