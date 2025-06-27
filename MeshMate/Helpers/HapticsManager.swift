d666ae80ff2deedd2f7185279b11573a256a2cde
Rodrigo Porto <rodrigo.porto@watchguard.com>
2025-06-27 00:04:34 -0300

    feat: add HapticsManager utility for reusable haptic feedback

---
+++ MeshMate/Helpers/HapticsManager.swift
@@ -0,0 +1,28 @@
+//
+//  HapticsManager.swift
+//  MeshMate
+//
+//  Created by Rodrigo Porto on 27/06/25.
+//
+
+import UIKit
+
+struct HapticsManager {
+    static func impact(style: UIImpactFeedbackGenerator.FeedbackStyle = .light) {
+        let generator = UIImpactFeedbackGenerator(style: style)
+        generator.prepare()
+        generator.impactOccurred()
+    }
+    
+    static func notification(type: UINotificationFeedbackGenerator.FeedbackType) {
+        let generator = UINotificationFeedbackGenerator()
+        generator.prepare()
+        generator.notificationOccurred(type)
+    }
+    
+    static func selection() {
+        let generator = UISelectionFeedbackGenerator()
+        generator.prepare()
+        generator.selectionChanged()
+    }
+}
