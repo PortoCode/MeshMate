6723a0f17cd0cc1a350d20a788213fe1da34e0d0
Rodrigo Porto <rodrigoandradeporto@hotmail.com>
2025-06-28 12:21:52 -0300

    feat: add RootView to move the navigation logic and states from MeshMateApp

--- MeshMate/MeshMateApp.swift
+++ MeshMate/MeshMateApp.swift
@@ -9,23 +9,10 @@ import SwiftUI
 
 @main
 struct MeshMateApp: App {
-    @StateObject private var auth = AuthManager()
-    @State private var didShowSplash = false
     
     var body: some Scene {
         WindowGroup {
-            if !didShowSplash {
-                SplashScreenView {
-                    didShowSplash = true
-                }
-            } else {
-                if auth.isLoggedIn {
-                    MainTabBar()
-                        .environmentObject(auth)
-                } else {
-                    LoginView(auth: auth)
-                }
-            }
+            RootView()
         }
     }
 }
---
+++ MeshMate/Views/RootView.swift
@@ -0,0 +1,28 @@
+//
+//  RootView.swift
+//  MeshMate
+//
+//  Created by Rodrigo Porto on 28/06/25.
+//
+
+import SwiftUI
+
+struct RootView: View {
+    @StateObject private var auth = AuthManager()
+    @State private var didShowSplash = false
+    
+    var body: some View {
+        if !didShowSplash {
+            SplashScreenView {
+                didShowSplash = true
+            }
+        } else {
+            if auth.isLoggedIn {
+                MainTabBar()
+                    .environmentObject(auth)
+            } else {
+                LoginView(auth: auth)
+            }
+        }
+    }
+}
