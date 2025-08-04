1053ac97bde7b111a94711ecd98dd1a2f8a166c0
Rodrigo Porto <rodrigoandradeporto@hotmail.com>
2025-08-03 22:41:04 -0300

    feat: add NetworkModePickerView

---
+++ MeshMate/Views/NetworkModePickerView.swift
@@ -0,0 +1,39 @@
+//
+//  NetworkModePickerView.swift
+//  MeshMate
+//
+//  Created by Rodrigo Porto on 03/08/25.
+//
+
+import SwiftUI
+
+struct NetworkModePickerView: View {
+    @Binding var mode: NetworkMode
+    
+    var body: some View {
+        VStack {
+            Text("Network Mode")
+                .font(.headline)
+                .padding(.horizontal)
+            Picker("Network Mode", selection: $mode) {
+                ForEach(NetworkMode.allCases) { mode in
+                    Text(mode.rawValue).tag(mode)
+                }
+            }
+            .pickerStyle(SegmentedPickerStyle())
+            .padding(.horizontal)
+        }
+    }
+}
+
+#Preview {
+    struct PreviewWrapper: View {
+        @State private var selectedMode: NetworkMode = .rest
+        
+        var body: some View {
+            NetworkModePickerView(mode: $selectedMode)
+        }
+    }
+    
+    return PreviewWrapper()
+}
